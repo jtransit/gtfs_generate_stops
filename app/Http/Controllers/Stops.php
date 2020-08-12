83d17ddf0268fe717665115f3d13b736bbf757cf
ivanrodgerapos <ivanrodgerapos@gmail.com>
2020-08-12 23:29:53 +0800

    Update Stops.php

--- app/Http/Controllers/Stops.php
+++ app/Http/Controllers/Stops.php
@@ -27,6 +27,7 @@ class Stops extends Controller
         $input_json = $request->input();
         $features = $input_json['features'];
         $coordinates = $features[0]['geometry']['coordinates'];
+        // $coordinates = $input_json['coordinates'];
         $endpoint = config('mapboxApi.endpoint.geocoding');
         $stops = [];
         $index = 0;
@@ -34,7 +35,10 @@ class Stops extends Controller
         foreach ($coordinates as $stop) {
             $longitude = $stop[0];
             $latitude = $stop[1];
-            $geolocation = $this->sendApiRequest($endpoint, $longitude, $latitude);
+            $geolocation = $this->sendApiRequest(
+                $endpoint, 
+                $longitude, 
+                $latitude);
             $stops[$index] = $this->genereate_stop_data_arr($geolocation);
             $index++;
         }
@@ -43,9 +47,16 @@ class Stops extends Controller
     }
 
     private function genereate_stop_data_arr($geolocation) {
+        if(isset($geolocation['features'][0]['properties']['address'])) {
+            $address = $geolocation['features'][0]['properties']['address'];
+            $address = str_replace(',', ' ', $address); // Remove any comma from address property
+        }
+
+        $feature_text = $geolocation['features'][0]['text'];
+        $stop_name = empty($address) ? $feature_text : $address;
+       
         $stop_id = uniqid();
         $stop_code = "";
-        $stop_name = $geolocation['features'][0]['properties']['address'];
         $stop_desc = "";
         $stop_lat = $geolocation['query'][1];
         $stop_lon = $geolocation['query'][0];
@@ -56,10 +67,8 @@ class Stops extends Controller
         $stop_timezone = "";
         $wheelchair_boarding = "";
 
-        $data = $stop_id.','.$stop_code.','.$stop_name.','.$stop_desc 
-                .','.$stop_lat.','.$stop_lon.','.$zone_id.','.$stop_url 
-                .','.$location_type.','.$parent_station.','.$stop_timezone
-                .','.$wheelchair_boarding;
+        $data = "$stop_id,$stop_code,$stop_name,$stop_desc,$stop_lat,$stop_lon,$zone_id,$stop_url,"
+                . "$location_type,$parent_station,$stop_timezone,$wheelchair_boarding";
                 
         return $data;
     }
@@ -67,7 +76,11 @@ class Stops extends Controller
     private function sendApiRequest($endpoint, $longitude, $latitude) {
         $mabox_URL = config('mapboxApi.url');
         $access_token = config('mapboxApi.access_token');
-        $request_URL = $mabox_URL . $endpoint . $longitude . ',' . $latitude . '.json?access_token=' . $access_token;
+        $request_URL = 
+            $mabox_URL 
+            . $endpoint 
+            . "$longitude,$latitude"
+            . ".json?access_token=$access_token";
         $response = Http::get($request_URL);
         return $response;
     }
